d2077a28baefc1c4d59f9776fd32696809114894
JakeOlst <jstainthorpe123@gmail.com>
2024-01-02 22:12:39 +0000

    Fixed JS Script for API Endpoint #1

--- scripts/background.js
+++ scripts/background.js
@@ -4,6 +4,7 @@ chrome.webNavigation.onBeforeNavigate.addListener(function (webURL) {
     queryGoogleWebRiskAPI(url);
 });
 
+/* Query API 1 - Google Web Risk API */
 function queryGoogleWebRiskAPI(url) {
     const apiEndpoint = 'https://webrisk.googleapis.com/v1/uris:search?threatTypes=MALWARE&threatTypes=SOCIAL_ENGINEERING&threatTypes=UNWANTED_SOFTWARE&threatTypes=SOCIAL_ENGINEERING_EXTENDED_COVERAE&uri=http%3A%2F%2Fexample.com&key=AIzaSyArDRynK0K_QY6F8LjVl_Z6Qqvx1Otry6g'
 
@@ -13,7 +14,7 @@ function queryGoogleWebRiskAPI(url) {
         console.log('Web Risk API Response:', data);
         if (data.threat) {
             const navURL = url;
-            chrome.webNavigation.onBeforeNavigate.removeListener(navToBlockPage);
+            // Adds new listener //
             chrome.webNavigation.onBeforeNavigate.addListener(navToBlockPage, { url: [{ urlContains: "blocked.html" }] });
         }
     })
@@ -24,8 +25,7 @@ function queryGoogleWebRiskAPI(url) {
 
 function navToBlockPage(details) {
     const redirectURL = chrome.extension.getURL('blockedPage.html');
-    chrome.webNavigation.onBeforeNavigate.removeListener(handleNavigation);
-    chrome.webNavigation.onBeforeNavigate.addListener(handleNavigation, { url: [{ urlContains: redirectURL }] });
+    chrome.webNavigation.onBeforeNavigate.removeListener(navToBlockPage);
     return { 
         redirectUrl: redirectURL
     };
