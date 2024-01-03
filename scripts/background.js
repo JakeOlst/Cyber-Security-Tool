6d4281d812d465ba9713e7d81e047b11a1a47ea0
JakeOlst <jstainthorpe123@gmail.com>
2024-01-03 09:48:37 +0000

    Scan 2 - Progress Save (incomplete)

--- scripts/background.js
+++ scripts/background.js
@@ -6,16 +6,53 @@ chrome.webNavigation.onBeforeNavigate.addListener(function (webURL) {
 
 /* Query API 1 - Google Web Risk API */
 function queryGoogleWebRiskAPI(url) {
-    const apiEndpoint = 'https://webrisk.googleapis.com/v1/uris:search?threatTypes=MALWARE&threatTypes=SOCIAL_ENGINEERING&threatTypes=UNWANTED_SOFTWARE&threatTypes=SOCIAL_ENGINEERING_EXTENDED_COVERAE&uri=http%3A%2F%2Fexample.com&key=AIzaSyArDRynK0K_QY6F8LjVl_Z6Qqvx1Otry6g'
+    const apiKey = '&key='+'AIzaSyArDRynK0K_QY6F8LjVl_Z6Qqvx1Otry6g';
+    const encodedUrl = '&uri='+encodeURIComponent(url);
+    const apiEndpoint = `https://webrisk.googleapis.com/v1/uris:search?threatTypes=MALWARE&threatTypes=SOCIAL_ENGINEERING&threatTypes=UNWANTED_SOFTWARE&threatTypes=SOCIAL_ENGINEERING_EXTENDED_COVERAGE`;
 
-    fetch(apiEndpoint)
+    fetch((apiEndpoint+encodedUrl+apiKey))
     .then(response => response.json())
     .then(data => {
-        console.log('Web Risk API Response:', data);
+        console.log("Submission to Google Web Risk API Successful");
+        console.log('Google Web Risk API Response:', data);
         if (data.threat) {
             const navURL = url;
             // Adds new listener //
             chrome.webNavigation.onBeforeNavigate.addListener(navToBlockPage, { url: [{ urlContains: "blocked.html" }] });
+        }
+    })
+    .catch(error => {
+        console.error('Error querying Google Web Risk API:', error);
+    });
+}
+
+/* Query API 2 - URL Scan IO */
+function queryURLScanIO(url) {
+    const apiEndpoint = 'https://urlscan.io/api/v1/search/?q=';
+    const apiKey = '9a05d09b-6284-41ae-97b0-0648173b00a4';
+
+    const postData = {
+        url: encodeURIComponent(url),
+        visibility: 'public',
+    }
+
+    fetch((apiEndpoint+encodedUrl), {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'API-Key': apiKey;
+        },
+        body: JSON.stringify(postData),
+    })
+    
+
+    .then(response => response.json())
+    .then(data => {
+        console.log("Submission to Google Web Risk API Successful");
+        if (data.uuid) {
+            console.log('Google Web Risk API Response:', data);
+            chrome.webNavigation.onBeforeNavigate.addListener(navToBlockPage, { url: [{ urlContains: "blocked.html" }] });
+        }
         }
     })
     .catch(error => {
