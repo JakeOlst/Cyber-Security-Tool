375c761d42a47cb3a144f4602e205be207ff7c84
JakeOlst <jstainthorpe123@gmail.com>
2024-01-27 18:36:31 +0000

    Pushed functionality for advertisement blocking.

---
+++ scripts/advertisementBlocking.js
@@ -0,0 +1,23 @@
+/*
+ * Ad Blocking Functionality using EasyList.
+*/
+
+let easyList = [];
+
+// Function to block ads on the page based on the locally stored EasyList
+function blockAdsOnPage() {
+    const elements = document.querySelectorAll('div, img, script, iframe, [src]');
+    elements.forEach(element => {
+        const src = element.src || element.getAttribute('src');
+        if (src && easyList.some(domain => src.includes(domain))) {
+            element.remove();
+            console.log("Ad Removed.");
+        }
+    });
+}
+
+chrome.storage.local.get('easyList', (result) => {
+    console.log("Result: "+result.easyList);
+    easyList = result.easyList;
+    blockAdsOnPage();
+});
