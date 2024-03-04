cc2ec3cef29ab9d77d78b9660e14f8f146fa830e
JakeOlst <jstainthorpe123@gmail.com>
2024-03-04 14:26:04 +0000

    Push fix for Payment Detection.

--- scripts/content/paymentInfoDetection.js
+++ scripts/content/paymentInfoDetection.js
@@ -3,10 +3,9 @@
  * Payment Info Detection 
 */
 
-// Regex for UK Credit Card providers (Visa, MasterCard, American Express, Discover, Maestro, Diners Club)
+// Regex for UK Credit Card providers (Visa, MasterCard, American Express, Maestro)
 // Source: https://ihateregex.io/expr/credit-card/
-const ukCreditCardRegex = /^(?:4[0-9]{3}\s?[0-9]{4}\s?[0-9]{4}\s?[0-9]{4}|4[0-9]{12}|5[1-5][0-9]{14}|3[47][0-9]{13}|(?:6011|65[0-9]{2}|64[4-9]|62212[6-9]|6221[3-9][0-9]|622[2-8][0-9]{2}|6229[01])\s?[0-9]{10}|(?:5[0678]\d\d|6304|6390|67\d\d)\d{8,15}|(?:5018|5020|5038|6304|6759|6761|6763)[0-9]{8,15}|(?:50[0-9]{4}|65[0-9]{2})\d{8,12})$/
-
+const ukCreditCardRegex = /^(?:4[0-9]{3}\s?[0-9]{4}\s?[0-9]{4}\s?[0-9]{4}|5[1-5][0-9]{2}\s?[0-9]{4}\s?[0-9]{4}\s?[0-9]{4}|3[47][0-9]{2}\s?[0-9]{6}\s?[0-9]{5}|(?:6011|65[0-9]{2}|64[4-9]|62212[6-9]|6221[3-9][0-9]|622[2-8][0-9]{2}|6229[01])\s?-[0-9]{4}\s?[0-9]{4}\s?[0-9]{4}|(?:5018|5020|5038|6304|6759|6761|6763)\s?[0-9]{4}\s?[0-9]{4}\s?[0-9]{4}\s?[0-9]{1,8}|(?:50[0-9]{2}|65[0-9]{2})\s?[0-9]{4}\s?[0-9]{4}\s?[0-9]{4}\s?[0-9]{1,8})$/
 let shouldBlockProceed = false;
 
 function checkNumberAgainstRegex(input) {
